6308bce1aa14317d9e3205e94dd6384ca6402ff5
xuhui.wei <xuhui.wei@bianlifeng.com>
2019-09-30 18:42:06 +0800

    add

--- common-java/common-web/src/main/java/common/web/spring/handler/JsonBodyConfigurationSelector.java
+++ common-java/common-web/src/main/java/common/web/spring/handler/JsonBodyConfigurationSelector.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import common.api.json.CodeMessage;
 import common.web.annotation.EnableJsonBody;
 import common.web.configuration.JsonBodyConfiguration;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
@@ -27,18 +28,30 @@ public class JsonBodyConfigurationSelector implements ImportSelector {
 			throw new IllegalArgumentException("@EnableJsonBody is not present on importing class " + annotationMetadata.getClassName());
 		}
 
-		String enableJsonBodyCode = enableJsonBody.getString("code");
-		String enableJsonBodyData = enableJsonBody.getString("data");
-		String enableJsonBodyMessage = enableJsonBody.getString("message");
+		configJsonSerializerMapper(enableJsonBody);
+
+		return new String[]{JsonBodyConfiguration.class.getName()};
+	}
+
+	/**
+	 * 配置JsonSerializer的mapper
+	 *
+	 * @param enableJsonBody
+	 */
+	private void configJsonSerializerMapper(AnnotationAttributes enableJsonBody) {
+
+		String code = enableJsonBody.getString("code");
+		String data = enableJsonBody.getString("data");
+		String message = enableJsonBody.getString("message");
+
+		CodeMessageSerializer codeMessageSerializer = new CodeMessageSerializer(code, data, message);
 
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
 		SimpleModule module = new SimpleModule();
-		module.addSerializer(CodeMessage.class, new CodeMessageSerializer(enableJsonBodyCode, enableJsonBodyData, enableJsonBodyMessage));
+		module.addSerializer(CodeMessage.class, codeMessageSerializer);
 		mapper.registerModule(module);
 
 		JsonSerializer.setMapper(mapper);
-
-		return new String[]{JsonBodyConfiguration.class.getName()};
 	}
 }
